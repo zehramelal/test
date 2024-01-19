0467da5ac11b12ef2aad1bca43df70818c6a3763
Zehra Melal <melalzehra@gmail.com>
2024-01-18 22:54:26 -0500

    v1

---
+++ second/second/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  second
+//
+//  Created by Zehra on 2024-01-09.
+//  Copyright © 2024 Zehra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
